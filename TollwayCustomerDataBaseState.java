73658c37bb6f2a729d13200a3e0c5416562e801b
ThatOneGuy560 <hochris950@hotmail.com>
2024-04-17 13:49:12 -0400

    Update TollwayCustomerDataBaseState.java

--- TollwayCustomerDataBaseState.java
+++ TollwayCustomerDataBaseState.java
@@ -5,11 +5,10 @@ import java.io.*;
 
 public class TollwayCustomerDataBaseState
    {
-    private Name name;
-    private CarInfo carInfo;
-    private Payment payment;
-   public static void main (String args []){
     
+   public static void main (String args []){
+    Name nice = new Name("Chris", "Ho");
+    System.out.println(nice);
    }
     
   
@@ -22,18 +21,36 @@ public class TollwayCustomerDataBaseState
    
 
 ////// Customer Attributes: Name, CarInfo, Payment
-class Customer  
+class Customer 
    {
-      
-//   public String getInfo()
+    private Name name;
+    private CarInfo carInfo;
+    private Payment payment;
+
+    public Customer(Name name, CarInfo carInfo, Payment payment){
+        this.name = name;
+        this.carInfo = carInfo;
+        this.payment = payment;
+    }
+   public String getInfo(){
+    return "Name: " + name.getInfo() + "\nCar Information: " + carInfo.getInfo() + "\nPayment: " + payment.getInfo();
+   }
    
    }
 
 ////// Name Attributes: first name, last name
 class Name
    {
-   
-//   public String getInfo()
+   private String firstName;
+   private String lastName;
+
+   public Name(String firstName, String lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+   }
+   public String getInfo(){
+    return firstName + " " + lastName;
+   }
   
 
    }
@@ -41,7 +58,15 @@ class Name
 ////// CarInfo Attributes: make, model, license_plates
 class CarInfo
    {
-     
+     private String make;
+     private String model;
+     private String license_plates;
+
+     public CarInfo(String make, String model, String license_plates){
+        this.make = make;
+        this.model = model;
+        this.license_plates = license_plates;
+     }
 //   public String getInfo()
   
    }
